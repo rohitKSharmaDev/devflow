8733dd2345ddf6fbc5bc0f919b673949525759e7
Rohit Sharma <rohitksharmadev@gmail.com>
2025-12-15 02:38:39 +0530

    Add fonts, CSS, assets and metadata for the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl underline">Welcome to DevFlow</h1>
+    <>
+      <h1 className="text-3xl underline">Welcome to DevFlow</h1>
+
+      <h1 className="text-3xl font-space-grotesk">Welcome to DevFlow</h1>
+    
+    </>
   );
 }
