82e036ffca239fb1ac66bffa009437fecdc49a25
Rohit Sharma <rohitksharmadev@gmail.com>
2025-12-18 11:09:37 +0530

    Create app main navbar and implement Light & Dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/ui/navigation/navbar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,9 +31,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
