8ef126e87048b15133f952d5abffdf2d966a805f
Rohit Sharma <rohitksharmadev@gmail.com>
2025-12-20 17:09:42 +0530

    finished Mobile Nav and added static pages

--- components/navbar/MobileNavaigation.tsx
+++ components/navbar/MobileNavaigation.tsx
@@ -1,3 +1,9 @@
+import { LogOut } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetClose,
@@ -6,12 +12,13 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import ROUTES from "@/constants/routes";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "../ui/button";
+
 import NavLinks from "./NavLinks";
 
-const MobileNavaigation = () => {
+const MobileNavigation = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -19,45 +26,73 @@ const MobileNavaigation = () => {
           src="/icons/hamburger.svg"
           width={36}
           height={36}
-          alt="Menu Icon"
-          className="invert-colors object-contain cursor-pointer sm:hidden"
+          alt="Menu"
+          className="invert-colors sm:hidden cursor-pointer"
         />
       </SheetTrigger>
       <SheetContent
-        className="background-light900_dark200 border-none"
         side="left"
+        className="background-light900_dark200 border-none"
       >
-        <div>
-          <SheetTitle className="hidden">Navigation</SheetTitle>
-          <Link href={"/"} className="flex items-center gap-1 mb-8">
-            <Image
-              src="/images/site-logo.svg"
-              width={23}
-              height={23}
-              alt="DevFlow Logo"
-            />
+        <SheetTitle className="hidden">Navigation</SheetTitle>
+        <Link href="/" className="flex items-center gap-1 mt-2 ml-3">
+          <Image
+            src="/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Logo"
+          />
 
-            <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
-              Dev<span className="text-primary-500">Flow</span>
-            </p>
-          </Link>
+          <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
+            Dev<span className="text-primary-500">Flow</span>
+          </p>
+        </Link>
 
-          <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
-            <SheetClose asChild>
-              <section className="flex h-full flex-col gap-6 pt-16">
-                <NavLinks isMobileNav />
-              </section>
-            </SheetClose>
+        <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
+          <SheetClose asChild>
+            <section className="flex h-full flex-col gap-6 pt-5 px-5">
+              <NavLinks isMobileNav />
+            </section>
+          </SheetClose>
 
-            <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 px-5">
+            {userId ? (
               <SheetClose asChild>
-                <Link href={ROUTES.SIGN_IN}>
-                  <Button className="small-medium btn-secondary min-h-10.25 w-full rounded-lg px-4 py-3 shadow-none">
-                    <span className="primary-text-gradient">Log In</span>
+                <form
+                  action={async () => {
+                    "use server";
+
+                    await signOut();
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    className="base-medium w-fit bg-transparent! px-4 py-3"
+                  >
+                    <LogOut className="size-5 text-black dark:text-white" />
+                    <span className="text-dark300_light900">Logout</span>
                   </Button>
-                </Link>
+                </form>
               </SheetClose>
-            </div>
+            ) : (
+              <>
+                <SheetClose asChild>
+                  <Link href={ROUTES.SIGN_IN}>
+                    <Button className="small-medium btn-secondary min-h-10.25 w-full rounded-lg px-4 py-3 mt-5 shadow-none">
+                      <span className="primary-text-gradient">Log In</span>
+                    </Button>
+                  </Link>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <Link href={ROUTES.SIGN_UP}>
+                    <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-10.25 w-full rounded-lg border px-4 py-3 shadow-none">
+                      Sign Up
+                    </Button>
+                  </Link>
+                </SheetClose>
+              </>
+            )}
           </div>
         </div>
       </SheetContent>
@@ -65,4 +100,4 @@ const MobileNavaigation = () => {
   );
 };
 
-export default MobileNavaigation;
+export default MobileNavigation;
--- components/navbar/NavLinks.tsx
+++ components/navbar/NavLinks.tsx
@@ -1,5 +1,56 @@
+'use client';
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { SheetClose } from "../ui/sheet";
+import React from "react";
+
 const NavLinks = ({ isMobileNav = false }: { isMobileNav: boolean}) => {
-  return <div>NavLinks</div>;
+  const pathname = usePathname();
+  const userId = 1;
+
+  return (
+    <>
+      {
+        sidebarLinks.map((item) => {
+          const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+
+          if(item.route === '/profile') {
+            if(userId) {
+              item.route = `${item.route}/${userId}`;
+            } else{
+              return null;
+            }
+          }
+
+          const LinkComponent = (
+            <Link href={item.route} key={item.label} className={cn(isActive ? 'primary-gradient rounded-lg text-light-900': 'text-dark300_light900', "flex items-center justify-start p-4")}>
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={cn({ "invert-colors": !isActive }, "mr-2")}
+              />
+
+              <p className={cn(isActive? 'base-bold': 'base-medium', !isMobileNav && "max-lg:hidden")}>{item.label}</p>
+            </Link>
+          );
+
+          return isMobileNav ? (
+            <SheetClose asChild key={item.route}>
+              {LinkComponent}
+            </SheetClose>
+          ) : (
+            <React.Fragment key={item.route}>{LinkComponent}</React.Fragment>
+          );
+        })
+      }
+    </>
+  );
 };
 
 export default NavLinks
