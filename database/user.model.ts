59fff2c96b874d8788bf75e6d1fa56a18f62dd84
Rohit Sharma <rohitksharmadev@gmail.com>
2025-12-25 18:56:09 +0530

    Database - User model refactored and implement Account model

--- database/user.model.ts
+++ database/user.model.ts
@@ -1,6 +1,5 @@
 import { model, models, Schema } from 'mongoose';
 
-
 export interface IUser {
   name: string;
   username: string;
@@ -12,17 +11,45 @@ export interface IUser {
   reputation?: number;
 }
 
-const UserSchema = new Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  bio: { type: String, default: '' }, // remove default after sometime
-  image: { type: String, required: true },
-  location: { type: String },
-  portfolio: { type: String },
-  reputation: { type: Number, default: 0 },
-
-}, { timestamps: true });
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+      default: "", // remove default after sometime
+    },
+    image: { 
+      type: String, 
+      required: true 
+    },
+    location: { 
+      type: String 
+    },
+    portfolio: { 
+      type: String 
+    },
+    reputation: { 
+      type: Number, 
+      default: 0 
+    },
+  },
+  { 
+    timestamps: true 
+  }
+);
 
 const User = models?.user || model<IUser>('User', UserSchema);
 
