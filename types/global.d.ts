249acbca166d640f1a062e3ebaf5270f00db514f
Rohit Sharma <rohitksharmadev@gmail.com>
2026-03-06 02:33:19 +0530

    Question Details - Implement get all answers action

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,34 +1,6 @@
-type ActionResponse<T = null> = {
-  success: boolean;
-  data?: T;
-  error?: {
-    message: string;
-    details?: Record<string, string[]>;
-  };
-  status?: number;
-};
-
-type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
-type ErrorResponse = ActionResponse<undefined> & { success: false };
-
-type APIErrorResponse = NextResponse<ErrorResponse>;
-type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
-
-interface UrlQueryParams {
-  params: string;
-  key: string;
-  value: string | null;
-}
-
-interface RemoveUrlQueryParams {
-  params: string;
-  keysToRemove: string[];
-}
-
 interface Tag {
   _id: string;
   name: string;
-  questions?: number;
 }
 
 interface Author {
@@ -45,20 +17,26 @@ interface Question {
   author: Author;
   createdAt: Date;
   upvotes: number;
-  downvotes: number;
   answers: number;
   views: number;
-}
-
-interface Answer {
-  _id: string;
-  author: Author;
-  content: string;
-  upvotes: number;
-  question: string;
-  downvotes: number;
   createdAt: Date;
 }
+
+type ActionResponse<T = null> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  status?: number;
+};
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+type ErrorResponse = ActionResponse<undefined> & { success: false };
+
+type APIErrorResponse = NextResponse<ErrorResponse>;
+type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
 
 interface RouteParams {
   params: Promise<Record<string, string>>;
@@ -73,77 +51,9 @@ interface PaginatedSearchParams {
   sort?: string;
 }
 
-interface Collection {
+interface Answer {
   _id: string;
-  author: string | Author;
-  question: Question;
-}
-
-interface User {
-  _id: string;
-  name: string;
-  username: string;
-  email: string;
-  bio?: string;
-  image?: string;
-  location?: string;
-  portfolio?: string;
-  reputation?: number;
+  author: Author;
+  content: string;
   createdAt: Date;
-}
-
-interface Badges {
-  GOLD: number;
-  SILVER: number;
-  BRONZE: number;
-}
-
-interface Job {
-  id?: string;
-  employer_name?: string;
-  employer_logo?: string | undefined;
-  employer_website?: string;
-  job_employment_type?: string;
-  job_title?: string;
-  job_description?: string;
-  job_apply_link?: string;
-  job_city?: string;
-  job_state?: string;
-  job_country?: string;
-}
-
-interface Country {
-  name: {
-    common: string;
-  };
-}
-
-interface GlobalSearchedItem {
-  id: string;
-  type: "question" | "answer" | "user" | "tag";
-  title: string;
-}
-
-type ActionResponse<T = null> = {
-  success: boolean;
-  data?: T;
-  error?: {
-    message: string;
-    details?: Record<string, string[]>;
-  };
-  status?: number;
-}
-
-type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
-type ErrorResponse = ActionResponse<undefined> & { success: false };
-
-type APIErrorResponse = NextResponse<ErrorResponse>;
-type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
-
-interface PaginatedSearchParams {
-  page?: number;
-  pageSize?: number;
-  query?: string;
-  filter?: string;
-  sort?: string;
 }
