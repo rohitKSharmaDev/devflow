79b77f56d49c75da77322c109a9df57746ca8709
Rohit Sharma <rohitksharmadev@gmail.com>
2026-03-12 17:05:10 +0530

    Question Details - implement Votes UI

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -4,6 +4,7 @@ import Preview from '@/components/editor/preview';
 import AnswerForm from '@/components/forms/AnswerForm';
 import Metric from '@/components/Metric';
 import UserAvatar from '@/components/UserAvatar';
+import Votes from '@/components/votes/Votes';
 import ROUTES from '@/constants/routes';
 import { getAnswers } from '@/lib/actions/answer.action';
 import { getQuestion, incrementViews } from '@/lib/actions/question.action';
@@ -56,7 +57,12 @@ const QuestionDetails = async ({ params} : RouteParams) => {
           </div>
 
           <div className="flex justify-end">
-            <p>Votes</p>
+            <Votes 
+              upvotes={question.upvotes} 
+              hasUpvoted={true} 
+              downvotes={question.downvotes} 
+              hasDownvVoted={false}
+            />
           </div>
         </div>
 
---
+++ components/votes/Votes.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { formatNumber } from '@/lib/utils';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { useState } from 'react'
+import { toast } from 'sonner';
+
+interface Props {
+  upvotes: number;
+  hasUpvoted: boolean;
+  downvotes: number;
+  hasDownvVoted: boolean;
+}
+
+const Votes = ({ upvotes, hasUpvoted, downvotes, hasDownvVoted}: Props) => {
+  const session = useSession();
+  const userId = session.data?.user?.id;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleVote = async(voteType: 'upvote' | 'downvote') => {
+    if(!userId) {
+      return toast.error("Please login to vote", {
+        description: "You need to be logged in to upvote or downvote a question."
+      });
+    }
+
+    try {
+      const successMessage = voteType === 'upvote' ? `Upvote ${!hasUpvoted ? "added": "removed"} successfully` : `Downvote ${!hasDownvVoted ? "added": "removed"} successfully`;
+
+      toast.success(successMessage, {
+        description: "Your vote has been recorded"
+      });
+      
+    } catch {
+      toast.error("Failed to vote", {
+        description: "An error occurred while voting"
+      });
+    
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex-center gap-2.5">
+      <div className="flex-center gap-1.5">
+        <Image
+          src={hasUpvoted ? "/icons/upvoted.svg" : "/icons/upvote.svg"}
+          alt="Upvote icon"
+          width={18}
+          height={18}
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          aria-label="Upvote"
+          onClick={() => !isLoading && handleVote("upvote")}
+        />
+
+        <div className="flex-center background-light700_datk400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(upvotes)}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex-center gap-1.5">
+        <Image
+          src={hasDownvVoted ? "/icons/downvoted.svg" : "/icons/downvote.svg"}
+          alt="Downvote icon"
+          width={18}
+          height={18}
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          aria-label="Downvote"
+          onClick={() => !isLoading && handleVote("downvote")}
+        />
+
+        <div className="flex-center background-light700_datk400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(downvotes)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Votes
