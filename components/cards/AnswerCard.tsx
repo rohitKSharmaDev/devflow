57b08e6718625ca4f4f0c0e12838010da336f8ee
Rohit Sharma <rohitksharmadev@gmail.com>
2026-03-16 15:36:15 +0530

    Question Details - implement answer votes in the UI

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import UserAvatar from '../UserAvatar'
 import Link from 'next/link'
 import ROUTES from '@/constants/routes'
 import { getTimeStamp } from '@/lib/utils'
 import Preview from '../editor/preview'
+import Votes from '../votes/Votes'
+import { hasVoted } from '@/lib/actions/vote.action'
 
-const AnswerCard = ({ _id, author, content, createdAt} : Answer) => {
+const AnswerCard = ({ _id, author, content, createdAt, upvotes, downvotes} : Answer) => {
+  const hasVotedPromise = hasVoted({
+    targetId: _id,
+    targetType: "answer",
+  });
+
   return (
     <article className="light-border border-b py-10">
       <span id={JSON.stringify(_id)} className="hash-span" />
@@ -27,21 +34,27 @@ const AnswerCard = ({ _id, author, content, createdAt} : Answer) => {
               {author.name ?? "Anonymous"}
             </p>
 
-            <p className='small-regular text-light400_light500 ml-0.5 mt-0.5 line-clamp-1'>
+            <p className="small-regular text-light400_light500 ml-0.5 mt-0.5 line-clamp-1">
               <span className="max-sm:hidden"> • </span>
               answered {getTimeStamp(createdAt)}
             </p>
           </Link>
         </div>
 
-        <div className='flex justify-end'>
-          Votes
+        <div className="flex justify-end">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Votes
+              targetType="answer"
+              targetId={_id}
+              upvotes={upvotes}
+              downvotes={downvotes}
+              hasVotedPromise={hasVotedPromise}
+            />
+          </Suspense>
         </div>
       </div>
 
-      <Preview
-        content={content}
-      />
+      <Preview content={content} />
     </article>
   );
 }
