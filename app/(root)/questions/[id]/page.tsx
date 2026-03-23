e03dad8a4d4f4348c72a0afb45dd09ceb74707f8
Rohit Sharma <rohitksharmadev@gmail.com>
2026-03-23 23:02:56 +0530

    Implement Save Question server action

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -15,6 +15,7 @@ import { getAnswers } from "@/lib/actions/answer.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { hasVoted } from "@/lib/actions/vote.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
+import SaveQuestion from "@/components/questions/SaveQuestion";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -71,6 +72,10 @@ const QuestionDetails = async ({ params }: RouteParams) => {
                 targetId={question._id}
                 hasVotedPromise={hasVotedPromise}
               />
+            </Suspense>
+
+            <Suspense fallback={<div>Loading...</div>}>
+              <SaveQuestion questionId={question._id} />
             </Suspense>
           </div>
         </div>
---
+++ components/questions/SaveQuestion.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { toggleSaveQuestion } from "@/lib/actions/collection.action";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
+import { toast } from "sonner";
+
+const SaveQuestion = ({ questionId }: { questionId: string}) => {
+  const session = useSession();
+  const userId = session.data?.user?.id;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSave = async () => {
+    if(isLoading) {
+      return;
+    }
+
+    if(!userId) {
+      return toast.error('You need to be logged in to save questions');
+    }
+    setIsLoading(true);
+
+    try {
+      const { success, data, error } = await toggleSaveQuestion({ questionId });
+
+      if(!success) {
+        throw new Error(error?.message || 'Failed to save the question');
+      }
+
+      toast.success(`Question ${data?.saved ? 'saved' : 'unsaved'} successfully`);
+      
+    } catch (error) {
+      toast.error('Error', {
+        description: error instanceof Error ? error.message : 'An error occurred while saving the question',
+      });
+    
+    } finally{
+      setIsLoading(false);
+    }
+
+  };
+
+  const hasSaved = false;
+
+  return (
+    <Image
+      src={hasSaved ? '/icons/star-filled.svg': '/icons/star-red.svg'}
+      width={18}
+      height={18}
+      alt="Save question Icon"
+      className={`cursor-pointer ${isLoading && 'opacity-50'}`}
+      aria-label="Save question"
+      onClick={handleSave}
+    />
+  );
+};
+
+export default SaveQuestion
