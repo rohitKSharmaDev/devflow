8b6c5688f5516637b4b2c0ab2bbff6116707591c
Rohit Sharma <rohitksharmadev@gmail.com>
2026-03-25 14:04:01 +0530

    Profile page - implement stats UI

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -6,6 +6,7 @@ import { notFound } from "next/navigation";
 import dayjs from "dayjs";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Stats from "@/components/user/Stats";
 
 
 const Profile = async ({ params}: RouteParams) => {
@@ -33,60 +34,72 @@ const Profile = async ({ params}: RouteParams) => {
   const { _id, name, image, portfolio, location, createdAt, username, bio } = user;
 
   return (
-    <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
-      <div className="flex flex-col items-start gap-4 lg:flex-row">
-        <UserAvatar
-          id={_id}
-          name={name}
-          imageUrl={image}
-          className="size-35 rounded-full object-cover"
-          fallbackClassName="text-6xl font-bolder"
-        />
-
-        <div className="mt-3">
-          <h2 className="h2-bold text-dark100_light900">{name}</h2>
-          <p className="patagraph-regular text-dark200_light800">@{username}</p>
-
-          <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
-            {portfolio && (
-              <ProfileLink
-                imgUrl="/icons/link.svg"
-                href={portfolio}
-                title="Portfolio"
-              />
-            )}
-
-            {location && (
-              <ProfileLink
-                imgUrl="/icons/location.svg"
-                title="Portfolio"
-              />
-            )}
-
-          <ProfileLink
-            imgUrl="/icons/calendar.svg"
-            title={dayjs(createdAt).format('MMMM YYYY')}
+    <>
+      <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <UserAvatar
+            id={_id}
+            name={name}
+            imageUrl={image}
+            className="size-35 rounded-full object-cover"
+            fallbackClassName="text-6xl font-bolder"
           />
-          </div>
 
-          {bio && (
-            <p className="paragraph-regular text-dark400_light800 mt-8">
-              {bio}
-            </p>
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{name}</h2>
+            <p className="patagraph-regular text-dark200_light800">@{username}</p>
+
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {portfolio && (
+                <ProfileLink
+                  imgUrl="/icons/link.svg"
+                  href={portfolio}
+                  title="Portfolio"
+                />
+              )}
+
+              {location && (
+                <ProfileLink
+                  imgUrl="/icons/location.svg"
+                  title="Portfolio"
+                />
+              )}
+
+            <ProfileLink
+              imgUrl="/icons/calendar.svg"
+              title={dayjs(createdAt).format('MMMM YYYY')}
+            />
+            </div>
+
+            {bio && (
+              <p className="paragraph-regular text-dark400_light800 mt-8">
+                {bio}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          {loggedInUser?.user?.id === id && (
+            <Link href="/profile/edit">
+              <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
+                Edit Profile
+              </Button>
+            </Link>
           )}
         </div>
-      </div>
+      </section>
 
-      <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
-        {loggedInUser?.user?.id === id && (
-          <Link href="/profile/edit">
-            <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
-              Edit Profile
-            </Button>
-          </Link>
-        )}
-      </div>
-    </section>
+      <Stats
+        totalQuestions={totalQuestions}
+        totalAnswers={totalAnswers}
+        badges={{
+          GOLD: 0,
+          SILVER: 0,
+          BRONZE: 0
+        }}
+      />
+    </>
   );
 }
 
--- types/global.d.ts
+++ types/global.d.ts
@@ -92,3 +92,9 @@ interface User {
   reputation?: number;
   createdAt: Date;
 }
+
+interface BadgeCounts {
+  GOLD: number;
+  SILVER: number;
+  BRONZE: number;
+}
